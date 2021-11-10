599382619e45b4941cd9052aae9a71000df5a97b
Novalnet <release@novalnetsolutions.com>
2021-11-10 15:25:18 +0100

    first commit

---
+++ NovalnetGatewayForm.ascx
@@ -0,0 +1,30 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="NovalnetGatewayForm.ascx.cs" Inherits="NovalnetGatewayForm" %>
+<h1>
+<asp:Label ID="paymentName" runat="server" EnableViewState="false" text="Novalnet" Visible="true" Font-Bold="true" Font-Size="11pt"/>
+</h1>
+<asp:Label ID="lblError" runat="server" EnableViewState="false" CssClass="ErrorLabel" Visible="true" Font-Bold="true" Font-Size="11pt"/>
+<h1 style="color:red"><asp:Label ID="testmode" EnableViewState="false" runat="server"  Visible="false" Font-Size="11pt" /></h1>
+<h1>
+<asp:Label ID="nnDescription" EnableViewState="false" runat="server"  Visible="true" Font-Size="11pt"/>
+    </h1>
+<asp:TextBox ID="txtAmount" runat="server" Visible="false" />
+ <asp:Button ID="nnContinueShopping"
+           Text="Continue Shopping"
+           OnClick="continueShoppingEvent" 
+           runat="server"
+     CssClass="continue_button"
+           Visible="true"/>
+<script>
+    jQuery(document).ready(function () {
+        jQuery("input[id$='_nnContinueShopping']").css('display','none');
+        if (jQuery('.ErrorLabel').html() != '') {
+            jQuery("input[id$='_btnProcessPayment']").css('display', 'none');
+            jQuery(".BlockContent").css('display', 'none');
+            jQuery(".wrapper > h1").html("Payment Failed");
+            jQuery("input[id$='_nnContinueShopping']").css('display', 'block');
+            jQuery("input[id$='_nnDescription']").css('display', 'none');
+            
+        }
+    });
+</script>
+
---
+++ NovalnetGatewayForm.ascx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using CMS.Ecommerce.Web.UI;
+using CMS.Helpers;
+using System.Web;
+using CMS.DataEngine;
+
+
+public partial class NovalnetGatewayForm : CMSPaymentGatewayForm
+{
+    /// <summary>
+    /// Called automatically after the related payment provider is assigned to the form and initialized.
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        // Attempts to get a default email address value from the Kentico customer object
+
+        string currentUrl = HttpContext.Current.Request.Url.AbsoluteUri.ToString();
+        if (currentUrl.Contains("novalnet_error_message="))
+        {
+            string[] parts = currentUrl.Split(new char[] { '?', '&' });
+            lblError.Text = Server.UrlDecode(parts[2].Replace("novalnet_error_message=", ""));
+            lblError.Visible = true;         
+        }
+        else
+        {
+            string shopTestMode = SettingsKeyInfoProvider.GetValue("test_mode");
+            if (shopTestMode == "True")
+            {
+                string testModeText = ResHelper.GetString("custom.testmode_notification");
+                testmode.Visible = true;
+                testmode.Text = testModeText;
+
+            }
+            string localizedResult = ResHelper.GetString("custom.payment_description");
+            nnDescription.Text = localizedResult;
+        }
+        
+    }
+
+    /// <summary>
+    /// Used for validation of the payment form's inputs.
+    /// </summary>
+    public override string ValidateData()
+    {
+        // The 'ValidateData' method of the base class delegates the validation logic to the related payment provider
+        // First gets the form's data using the 'GetPaymentGatewayData()' method
+        // and then validates it by calling the provider's 'ValidateCustomData' method
+        string error = base.ValidateData();
+
+        if (!string.IsNullOrEmpty(error))
+        {
+            lblError.Visible = true;
+            lblError.Text = error;
+        }
+
+        return error;
+    }
+
+    /// <summary>
+    /// Redirect to continue shopping page
+    /// </summary>
+    protected void continueShoppingEvent(object sender, EventArgs e)
+    {
+        URLHelper.Redirect(ResolveUrl("~/default.aspx"));
+    }
+}
